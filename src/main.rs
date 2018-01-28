31e589586a65d5b2fc2c6c15567b89f1d53654bc
garyemerson <gmohammadioun@gmail.com>
2018-01-27 23:47:57 -0800

    lesson 1 complete!

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,86 @@
+extern crate regex;
 extern crate image;
 
 mod img;
 use img::Img;
 
+use std::io::BufReader;
+use std::fs::File;
+use regex::Regex;
+use std::io::BufRead;
+use std::mem;
+
 fn main() {
-    let mut img = Img::new(100, 100);
+    let width: f32 = 800.0;
+    let height: f32 = 800.0;
+    let mut img = Img::new(width as u32, height as u32);
 
-    for _ in 0..1000000 {
-        // line(13, 20, 80, 40, &mut img, (255, 255, 255));
+    // for _ in 0..1000000 {
+    //     line4(50, 50, 50, 60, &mut img, (255, 255, 255));
+    //     line4(50, 50, 50, 40, &mut img, (255, 255, 255));
+    //     line4(50, 50, 60, 60, &mut img, (255, 255, 255));
+    //     line4(50, 50, 60, 50, &mut img, (255, 255, 255));
+    //     line4(50, 50, 40, 50, &mut img, (255, 255, 255));
+    //     line4(50, 50, 60, 40, &mut img, (255, 255, 255));
+    //     line4(50, 50, 40, 60, &mut img, (255, 255, 255));
+    //     line4(50, 50, 40, 40, &mut img, (255, 255, 255));
+    // }
 
-        // line2(13, 20, 80, 40, &mut img, (255, 255, 255));
-
-        // line2(13, 20, 80, 40, &mut img, (255, 255, 255)); 
-        // line2(20, 13, 40, 80, &mut img, (255, 0, 0)); 
-        // line2(80, 40, 13, 20, &mut img, (255, 0, 0));
-
-        // line3(50, 50, 50, 60, &mut img, (255, 255, 255));
-        // line3(50, 50, 50, 40, &mut img, (255, 255, 255));
-        // line3(50, 50, 60, 60, &mut img, (255, 255, 255));
-        // line3(50, 50, 60, 50, &mut img, (255, 255, 255));
-        // line3(50, 50, 40, 50, &mut img, (255, 255, 255));
-        // line3(50, 50, 60, 40, &mut img, (255, 255, 255));
-        // line3(50, 50, 40, 60, &mut img, (255, 255, 255));
-        // line3(50, 50, 40, 40, &mut img, (255, 255, 255));
-
-        // line3(13, 20, 80, 40, &mut img, (255, 255, 255)); 
-        // line3(20, 13, 40, 80, &mut img, (255, 0, 0)); 
-        // line3(80, 40, 13, 20, &mut img, (255, 0, 0));
-
-        line4(13, 20, 80, 40, &mut img, (255, 255, 255)); 
-        line4(20, 13, 40, 80, &mut img, (255, 0, 0)); 
-        line4(80, 40, 13, 20, &mut img, (255, 0, 0));
+    let (vertices, faces) = parse_obj_file("/Users/Garrett/Dropbox/Files/workspaces/tinyrenderer_rust/african_head.obj");
+    println!("{} vertices, {} faces", vertices.len(), faces.len());
+    for f in faces {
+        line4(
+            ((vertices[(f.0 - 1) as usize].0 + 1.0) * width / 2.0).floor() as u32,
+            ((vertices[(f.0 - 1) as usize].1 + 1.0) * height / 2.0).floor() as u32,
+            ((vertices[(f.1 - 1) as usize].0 + 1.0) * width / 2.0).floor() as u32,
+            ((vertices[(f.1 - 1) as usize].1 + 1.0) * height / 2.0).floor() as u32,
+            &mut img, (255, 255, 255));
+        line4(
+            ((vertices[(f.1 - 1) as usize].0 + 1.0) * width / 2.0).floor() as u32,
+            ((vertices[(f.1 - 1) as usize].1 + 1.0) * height / 2.0).floor() as u32,
+            ((vertices[(f.2 - 1) as usize].0 + 1.0) * width / 2.0).floor() as u32,
+            ((vertices[(f.2 - 1) as usize].1 + 1.0) * height / 2.0).floor() as u32,
+            &mut img, (255, 255, 255));
+        line4(
+            ((vertices[(f.2 - 1) as usize].0 + 1.0) * width / 2.0).floor() as u32,
+            ((vertices[(f.2 - 1) as usize].1 + 1.0) * height / 2.0).floor() as u32,
+            ((vertices[(f.0 - 1) as usize].0 + 1.0) * width / 2.0).floor() as u32,
+            ((vertices[(f.0 - 1) as usize].1 + 1.0) * height / 2.0).floor() as u32,
+            &mut img, (255, 255, 255));
     }
 
-    // img.flip_vertical();
+    img.flip_vertical();
     img.save("output.png");
 }
 
 fn line4(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32, img: &mut Img, color: (u8, u8, u8)) {
     let mut steep = false; 
     if ((x0 as i32) - (x1 as i32)).abs() < ((y0 as i32) - (y1 as i32)).abs() { // if the line is steep, we transpose the image 
-        let mut temp = x0;
-        x0 = y0;
-        y0 = temp;
-
-        temp = x1;
-        x1 = y1;
-        y1 = temp;
-
+        mem::swap(&mut x0, &mut y0);
+        mem::swap(&mut x1, &mut y1);
         steep = true;
     } 
-
     if x0 > x1 {
-        let mut temp = x1;
-        x1 = x0;
-        x0 = temp;
-
-        temp = y1;
-        y1 = y0;
-        y0 = temp;
+        mem::swap(&mut x0, &mut x1);
+        mem::swap(&mut y0, &mut y1);
     }
+
+    println!("drawing line ({}, {}) ({}, {})", x0, y0, x1, y1);
 
     let dx = ((x0 as i32) - (x1 as i32)).abs();
     let dy = ((y0 as i32) - (y1 as i32)).abs();
-    // let dydx: f64 = (dy as f64) / (dx as f64);
-    // let mut error = 0.0;
     let mut y = y0;
     let mut error2: i32 = 0;
 
     for x in x0..(x1 + 1) {
         if steep {img.put(y as u32, x as u32, color); } else {img.put(x as u32, y as u32, color);}
 
-        // error += dydx;
-        // error2 = error * (dx as f64);
         if 2 * (error2 + dy) < dx {
-            // error = error + dydx;
             error2 = error2 + dy;
         } else {
-            y += 1;
-            // error * (dx as f64)
-            error2 = error2 + dy - dx;
+            // TODO: somehow y is becoming negative with diablo3 wirefram
+            y = if y1 > y0 { y + 1 } else { y - 1 };
+            error2 = error2 + dy - dx; 
         }
     }
 }
@@ -87,25 +88,13 @@ fn line4(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32, img: &mut Img, colo
 fn line3(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32, img: &mut Img, color: (u8, u8, u8)) {
     let mut steep = false; 
     if ((x0 as i32) - (x1 as i32)).abs() < ((y0 as i32) - (y1 as i32)).abs() { // if the line is steep, we transpose the image 
-        let mut temp = x0;
-        x0 = y0;
-        y0 = temp;
-
-        temp = x1;
-        x1 = y1;
-        y1 = temp;
-
+        mem::swap(&mut x0, &mut y0);
+        mem::swap(&mut x1, &mut y1);
         steep = true;
     } 
-
     if x0 > x1 {
-        let mut temp = x1;
-        x1 = x0;
-        x0 = temp;
-
-        temp = y1;
-        y1 = y0;
-        y0 = temp;
+        mem::swap(&mut x0, &mut x1);
+        mem::swap(&mut y0, &mut y1);
     }
 
     let dx = ((x0 as i32) - (x1 as i32)).abs();
@@ -132,25 +121,13 @@ fn line3(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32, img: &mut Img, colo
 fn line2(mut x0: u32, mut y0: u32, mut x1: u32, mut y1: u32, img: &mut Img, color: (u8, u8, u8)) {
     let mut steep = false; 
     if ((x0 as i32) - (x1 as i32)).abs() < ((y0 as i32) - (y1 as i32)).abs() { // if the line is steep, we transpose the image 
-        let mut temp = x0;
-        x0 = y0;
-        y0 = temp;
-
-        temp = x1;
-        x1 = y1;
-        y1 = temp;
-
+        mem::swap(&mut x0, &mut y0);
+        mem::swap(&mut x1, &mut y1);
         steep = true;
     } 
-
     if x0 > x1 {
-        let mut temp = x1;
-        x1 = x0;
-        x0 = temp;
-
-        temp = y1;
-        y1 = y0;
-        y0 = temp;
+        mem::swap(&mut x0, &mut x1);
+        mem::swap(&mut y0, &mut y1);
     }
 
     for x in x0..(x1 + 1) {
@@ -172,4 +149,39 @@ fn line(x0: u32, y0: u32, x1: u32, y1: u32, img: &mut Img, color: (u8, u8, u8)) 
         let y = (y0 as f64) * (1f64 - t) + (y1 as f64) * t;
         img.put(x.round() as u32, y.round() as u32, color);
     }
+}
+
+// Returns (vertices, faces)
+fn parse_obj_file(path: &str) -> (Vec<(f32, f32, f32)>, Vec<(u32, u32, u32)>) {
+    let f = File::open(path).unwrap();
+    let mut buffer = BufReader::new(f);
+
+    let mut vertices = Vec::new();
+    let mut faces = Vec::new();
+
+    let vre = Regex::new(r"v ([\d\-\.e]+) ([\d\-\.e]+) ([\d\-\.e]+)").unwrap();
+    let fre = Regex::new(r"f (\d+)/[^ ]+ (\d+)/[^ ]+ (\d+)/[^ ]+").unwrap();
+
+    for l in buffer.lines() {
+        let l = l.unwrap();
+        if l.starts_with("v ") {
+            for cap in vre.captures_iter(&l) {
+                let v1 = &cap[1].trim();
+                let v2 = &cap[2].trim();
+                let v3 = &cap[3].trim();
+                let v = (v1.parse::<f32>().unwrap(), v2.parse::<f32>().unwrap(), v3.parse::<f32>().unwrap());
+                vertices.push(v);
+            }
+        } else if l.starts_with("f ") {
+            for cap in fre.captures_iter(&l) {
+                let f1 = &cap[1].trim();
+                let f2 = &cap[2].trim();
+                let f3 = &cap[3].trim();
+                let f = (f1.parse::<u32>().unwrap(), f2.parse::<u32>().unwrap(), f3.parse::<u32>().unwrap());
+                faces.push(f);
+            }
+        }
+    }
+
+    (vertices, faces)
 }
